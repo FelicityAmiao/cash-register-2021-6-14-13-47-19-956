acb8353ae63c1652fac3e1f14a642144dc217919
CHENFE2 <felicity.chen@cargosmart.com>
2021-06-17 01:02:28 +0800

    [Felicity]test: When CashRegister process, will print purchase info

--- src/test/java/com/tw/academy/CashRegisterTest.java
+++ src/test/java/com/tw/academy/CashRegisterTest.java
@@ -4,6 +4,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 
@@ -18,6 +19,21 @@ class CashRegisterTest {
 		//then
 		//verify that cashRegister.process will trigger print
 		verify(spyPrinter, times(1)).print(anyString());
+	}
+	
+	@Test
+	void should_print_with_purchase_when_call_process() {
+		//given
+		Printer spyPrinter = mock(Printer.class);
+		CashRegister cashRegister = new CashRegister(spyPrinter);
+		Purchase stubPurchase = mock(Purchase.class);
+		String expected = "Buy an apply";
+		when(stubPurchase.asString()).thenReturn(expected);
+		//when
+		cashRegister.process(stubPurchase);
+		//then
+		//verify that cashRegister.process will trigger print with purchase
+		verify(spyPrinter, times(1)).print(expected);
 	}
 
 }
